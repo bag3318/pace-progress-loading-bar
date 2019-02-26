7dedb10a6b043cf5cc5047d87fbc36722dd4b6da
bag3318 <bglatman@outlook.com>
2019-02-25 16:18:23 -0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 "use strict";
 
+
 let logNodePackageName = (nodePackageName) => {
     console.log(nodePackageName);
 }
+
 const nodePackageName = 'Pace Loading/Progress Bar';
+
 logNodePackageName(nodePackageName);
